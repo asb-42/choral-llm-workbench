07f484756894759266698a9bb5251ae5bf78db70
asb <asb@kefk.org>
2026-01-05 00:53:26 +0100

    fix: export update_single_measure in core.score

--- core/score/__init__.py
+++ core/score/__init__.py
@@ -2,4 +2,9 @@ from .parser import load_musicxml
 from .writer import write_musicxml
 from .voice_detection import detect_voices
 from .harmony import analyze_chords
-from .reharmonize import replace_chord_in_measure, replace_chords_in_measures, apply_llm_chords_to_measures
+from .reharmonize import (
+    replace_chord_in_measure,
+    replace_chords_in_measures,
+    apply_llm_chords_to_measures,
+    update_single_measure,  
+)
